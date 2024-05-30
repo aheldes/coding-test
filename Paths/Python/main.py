fbabd55f99703b3fdc855a66ce61ecd80a7a2adc
aheldes <adrian.heldes@telekom.com>
2024-05-30 18:01:09 +0200

    feat: Add Paths findings algo

---
+++ Paths/Python/main.py
@@ -0,0 +1,57 @@
+from pydantic import BaseModel
+
+
+class GNode(BaseModel):
+    name: str
+    children: list['GNode'] = []
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_children(self) -> list['GNode']:
+        return self.children
+
+    # Implement __hash__ to be able to compare GNode objects as list is not hashable by default
+    def __hash__(self) -> int:
+        return hash((self.name, tuple(self.children)))
+
+
+def get_paths(node: GNode) -> list[list[GNode]]:
+    found_paths: list[list[GNode]] = []
+
+    def _find_path(_node: GNode, _path: list[GNode]) -> None:
+        # Add current node to the path
+        _path = _path + [_node]
+
+        # Base case
+        if not _node.get_children():
+            found_paths.append(_path)
+        else:
+            for child in _node.get_children():
+                # Recursively find paths
+                _find_path(child, _path)
+
+    _find_path(node, [])
+    return found_paths
+
+
+if __name__ == "__main__":
+    # Create dummy nodes
+    node_J = GNode(name="J")
+    node_I = GNode(name="I")
+    node_H = GNode(name="H")
+    node_G = GNode(name="G")
+    node_F = GNode(name="F")
+    node_E = GNode(name="E")
+
+    node_D = GNode(name="D", children=[node_J])
+    node_C = GNode(name="C", children=[node_G, node_H, node_I])
+    node_B = GNode(name="B", children=[node_E, node_F])
+
+    main_node = GNode(name="A", children=[node_B, node_C, node_D])
+
+    # Find paths
+    result = get_paths(main_node)
+    print(f"Found paths: {result}")
+
+    print(f"String representation of paths: {[[node.get_name() for node in path] for path in result]}")
---
+++ Paths/TS/main.ts
@@ -0,0 +1,62 @@
+interface IGNode {
+    getName(): string
+
+    getChildren(): IGNode[]
+}
+
+class GNode implements IGNode {
+    private readonly name: string
+    private readonly children: IGNode[]
+
+    constructor(name: string, children?: IGNode[]) {
+        this.name = name
+        this.children = children || []
+    }
+
+    public getName(): string {
+        return this.name
+    }
+
+    public getChildren(): IGNode[] {
+        return this.children
+    }
+}
+
+const paths = (node: IGNode): IGNode[][] => {
+    const foundPaths: IGNode[][] = []
+
+    const findPath = (node: IGNode, path: IGNode[]): void => {
+        const newPath = [...path, node]
+
+        if (node.getChildren().length === 0) {
+            foundPaths.push(newPath)
+            return
+        }
+
+        node.getChildren().forEach(child => findPath(child, newPath))
+    }
+
+    findPath(node, [])
+
+    return foundPaths
+
+}
+
+const nodeJ = new GNode("J")
+const nodeI = new GNode("I")
+const nodeH = new GNode("H")
+const nodeG = new GNode("G")
+const nodeF = new GNode("F")
+const nodeE = new GNode("E")
+
+const nodeD = new GNode("D", [nodeJ])
+const nodeC = new GNode("C", [nodeG, nodeH, nodeI])
+const nodeB = new GNode("B", [nodeE, nodeF])
+
+const mainNode = new GNode("A", [nodeB, nodeC, nodeD])
+
+// Find paths
+const result = paths(mainNode)
+console.log(`Found paths: ${result}`)
+
+console.log(`String representation of paths: ${JSON.stringify(result.map(path => path.map(node => node.getName())))}`)
